f5f8772e245458e7bf3b2d2a585ede24d4a5929e
Bhagwati-admin <bhagwatijewels501@gmail.com>
2026-08-19 22:06:24 +0530

    Add Supabase config

---
+++ config.js
@@ -0,0 +1,5 @@
+/* Filled in at deploy time */
+window.BJ_CONFIG = {
+  SUPABASE_URL: 'https://njikksnabvdmzynepdgw.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qaWtrc25hYnZkbXp5bmVwZGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMzc2MTAsImV4cCI6MjEwMjcxMzYxMH0.xHUkKn_zs8Ay4VljWAv2v0w5LoX9VRnKczc7tHN3zCU'
+};
